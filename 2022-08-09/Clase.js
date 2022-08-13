0c5d9633ab98388fb46642081a116759ed5f2b02
Eugenio Regis <kuque84@hotmail.com>
2022-08-13 13:12:52 -0300

    y

---
+++ 2022-08-09/Clase.js
@@ -0,0 +1,29 @@
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 30,
+    stock: "500",
+    disponible: true,
+    imagen: "imagen.jpg"
+}
+const cliente ={
+    nombre: "Eugenio",
+    aplelido: "Regis",
+    dni: "30711347",
+}
+const vendedor={
+    nombre: "Alan",
+    aplelido: "Abt",
+    dni: "00000000"
+}
+
+//console.log("Nombre: "+ producto.nombre);
+//console.log("Precio: "+ producto.precio);
+//console.log("Disponibilidad: "+ producto.disponible);
+//console.log("Stock: "+ producto.stock);
+
+console.log("Producto:");
+console.log(producto);
+console.log("Cliente:");
+console.log(cliente);
+console.log("Vendedor:");
+console.log(vendedor);
