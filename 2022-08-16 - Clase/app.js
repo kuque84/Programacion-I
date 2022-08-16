db3253afc139aa017cfa9d7c96e53ccd06d1e7c9
Eugenio Regis <kuque84@hotmail.com>
2022-08-16 18:05:24 -0300

    y

--- 2022-08-16 - Clase/app.js
+++ 2022-08-16 - Clase/app.js
@@ -3,13 +3,24 @@ const producto = {
     precio: 30,
     disponible: true,
     informacion : {
-        peso: '1kg',
-        medida: '1m',
-             fabricacion :{
-                 china :"china",
-                 anio :"2015",
-             }
-    }
+        medidas :{
+            peso: '1kg',
+            medida: '1m',
+        },
+        fabricacion:{
+            pais :'China',
+            anio :'2011',
+        }
+    } 
+
 }
 
-console.log(producto.informacion.fabricacion.anio)
+
+const {nombre, informacion, informacion:{peso}} = producto;
+
+//console.log(producto);
+//console.log(producto.informacion.medidas.peso);
+//console.log(informacion.peso);
+//console.log(informacion.medidas.medida);
+//console.log(nombre);
+//console.log(informacion.fabricacion);
