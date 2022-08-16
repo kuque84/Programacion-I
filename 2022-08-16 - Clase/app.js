0302f697aa8bb625769c0098a0606877f6134ef5
Eugenio Regis <kuque84@hotmail.com>
2022-08-16 18:49:19 -0300

    y

--- 2022-08-16 - Clase/app.js
+++ 2022-08-16 - Clase/app.js
@@ -1,3 +1,4 @@
+'use strict';
 const producto = {
     nombre: "Monitor 20 pulgadas",
     precio: 30,
@@ -14,13 +15,18 @@ const producto = {
     } 
 
 }
-
-
 const {nombre, informacion, informacion:{peso}} = producto;
 
-//console.log(producto);
+console.log(producto);
 //console.log(producto.informacion.medidas.peso);
 //console.log(informacion.peso);
 //console.log(informacion.medidas.medida);
 //console.log(nombre);
 //console.log(informacion.fabricacion);
+Object.seal(producto);
+producto.disponible = false;
+producto.precio = 40;
+console.log(producto.disponible);
+console.log(producto.precio);
+console.log(Object.isFrozen(producto));
+console.log(Object.isSealed(producto));
