dea95b1fc9d11243e27eaa4e8c408e8309e937bf
Eugenio Regis <kuque84@hotmail.com>
2022-08-21 11:15:38 -0300

    y

---
+++ 2022-08-19/Clase.js
@@ -0,0 +1,36 @@
+'use strict';
+/* deconstruir  */
+/*
+const Empleado ={
+    nombre: "Eugenio",
+    apellido: "Regis",
+    dni: "30711347",
+    direccion: "Fatima 261",
+    localidad: "Villa Ciudad Parque",
+    seccion: "Ventas",
+}
+
+const { nombre: nom, apellido: ape, dni: doc, seccion: area} = Empleado;
+
+console.log (Empleado);
+console.log (`${nom} ${ape}, ${doc} - ${area}`);
+*/
+
+/*  assing and spread   */
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 30,
+    disponible: true,
+}
+const medidas = {
+    peso: '1kg',
+    medida: '1m'
+}
+const fabricacion={
+    pais :'China',
+    anio :'2011'
+}
+
+const resultado = {...producto, ...medidas, ...fabricacion};
+
+console.log(resultado);
