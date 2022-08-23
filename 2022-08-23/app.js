70b25db15fe3e38639db28ee3c3059b73825f34e
Eugenio Regis <kuque84@hotmail.com>
2022-08-23 18:10:12 -0300

    y

---
+++ 2022-08-23/app.js
@@ -0,0 +1,6 @@
+'use strict';
+const deTodo = ["Hola", 10, true, "si", null, {nombre:'Juan', trabajo: 'Programador'}, [1, 2, 3,4]];
+
+console.log(deTodo);
+console.log(deTodo[5]);
+console.log(deTodo[5].nombre);
