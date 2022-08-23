3d45b1460ecca5fc85b40710910493bc1f329c5b
Eugenio Regis <kuque84@hotmail.com>
2022-08-23 18:21:38 -0300

    y

--- 2022-08-23/app.js
+++ 2022-08-23/app.js
@@ -1,6 +1,8 @@
 'use strict';
+const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
 const deTodo = ["Hola", 10, true, "si", null, {nombre:'Juan', trabajo: 'Programador'}, [1, 2, 3,4]];
 
 console.log(deTodo);
-console.log(deTodo[5]);
+console.log(deTodo[3]);
 console.log(deTodo[5].nombre);
+console.log(meses.length);
